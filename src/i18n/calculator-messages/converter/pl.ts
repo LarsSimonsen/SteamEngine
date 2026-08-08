da244721359fb3c3332709bd019d0efe39483edd
Lars Simonsen <lars.simonsen@gmail.com>
2026-08-08 04:35:39 +0200

    feat(i18n): add Polish locale

---
+++ src/i18n/calculator-messages/converter/pl.ts
@@ -0,0 +1,29 @@
+import type { CalculatorRouteMessages } from "../../calculator-messages";
+
+export const messages =  {
+    "eyebrow": "Konwersja drutu i wymiarów",
+    "title": "Konwerter",
+    "description": "Konwertuj milimetry, cale, AWG, BWG, SWG i cale ułamkowe.",
+    "embeddedDescription": "Konwersja pomiędzy milimetrami, calami, średnicami drutu i ułamkami cali.",
+    "input": "Wejście",
+    "value": "Wartość",
+    "inputValue": "Wartość wejściowa",
+    "inputUnit": "Jednostka wejściowa",
+    "result": "Wynik",
+    "millimetres": "Milimetry",
+    "inches": "Cale",
+    "americanWireGauge": "Amerykański miernik drutu",
+    "needleGauge": "BWG (wskaźnik igły)",
+    "standardWireGauge": "Standardowy miernik drutu",
+    "fractionalInches": "Ułamkowe cale",
+    "reset": "Zresetuj ustawienia domyślne",
+    "calculating": "Obliczanie…",
+    "betweenStandardValues": "Pomiędzy wartościami standardowymi",
+    "larger": "większy",
+    "smaller": "mniejszy",
+    "swap": "Zamień wejście i wyjście",
+    "outputSettings": "Ustawienia wyjściowe",
+    "outputUnit": "Jednostka wyjściowa",
+    "decimalPlaces": "Miejsca dziesiętne",
+    "precisionHelp": "Precyzja dotyczy wyników numerycznych. Tabele wskaźników pozostają przy opublikowanych wartościach."
+} satisfies CalculatorRouteMessages;
